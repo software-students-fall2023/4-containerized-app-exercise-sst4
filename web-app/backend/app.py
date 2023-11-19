776e5fdab20d4be8e72b2144f7e9f89cfb0fc8ad
Silver1793 <richardli1793@gmail.com>
2023-11-19 16:16:11 -0500

    test 5

--- web-app/backend/app.py
+++ web-app/backend/app.py
@@ -1,17 +1,17 @@
 """
-Simple Flask application.
+This is the app.py boiler plate
 """
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
     """
     Test Route
     """
-    return 'Hello, World!'
+    return "Hello, World!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
