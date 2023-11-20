b695f82e6cca0252a73691dda24565d253661d82
Ryan Zhang <zhangryan09@gmail.com>
2023-11-20 02:53:45 -0500

    lint fixes

--- database/db.py
+++ database/db.py
@@ -1,9 +1,10 @@
+"""
+Connects to the MongoDB database
+"""
+
 import os
 
-import pymongo
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-import dotenv
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,12 +12,12 @@ load_dotenv()
 # Create a new client and connect to the server
 uri = os.getenv('MONGODB_URI').format(os.getenv('MONGODB_USER'), os.getenv('MONGODB_PASSWORD'))
 client = MongoClient(uri, serverSelectionTimeoutMS = 3000)
-db = None
+DB = None
 
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    db = client[os.getenv('MONGODB_DATABASE')]
+    DB = client[os.getenv('MONGODB_DATABASE')]
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
