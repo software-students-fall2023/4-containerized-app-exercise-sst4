b5758e6569bf48da3f2f44fbe94f50fa0fb1f55c
BradFeng02 <bradfeng02@gmail.com>
2023-11-21 07:23:28 -0500

    little itty bitty fixes

--- web-app/app.py
+++ web-app/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     """main page (only page)"""
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
