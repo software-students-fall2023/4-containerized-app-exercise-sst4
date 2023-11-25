47898f463ab8d68f83316c0215f739e7bdd18827
Silver1793 <richardli1793@gmail.com>
2023-11-25 13:42:12 -0500

    fixed small bug where return was not out of for loop

--- machineLearningClient/recognition.py
+++ machineLearningClient/recognition.py
@@ -4,18 +4,39 @@ import base64
 import cv2
 import face_recognition
 import numpy as np
+from PIL import Image
+import io
+from io import BytesIO
+from database.db import db
+import binascii
 
-def recognize_user(image_base64, user_data):
-    try:
-        known_image = face_recognition.load_image_file(image_base64)
+def recognize_user(user):
+    # try:
+        image_bytes = base64.b64decode(user.split(',')[1]) # Takes out the data:image/jpeg;base64, since frontend sends as jpeg
 
+        image_np = face_recognition.api.load_image_file(io.BytesIO(image_bytes)) # Loads the file as a bytefile
 
-        biden_encoding = face_recognition.face_encodings(known_image)[0]
-        unknown_encoding = face_recognition.face_encodings(user_data)[0]
+        face_encodings = face_recognition.face_encodings(image_np) #Does the libraries encoding preparing for comaprison
 
-        results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+        if not face_encodings:
+            return {'message': 'No faces found in the captured image.'}
 
-        return results
+        usersCollection = db["users"]
+        usersFind = usersCollection.find({}) # Finds all users
+        for document in usersFind: #Cycles through all users
+            reference_image = document["image"] # Gets the image for each user
 
-    except Exception as e:
-        return {"recognized": False, "name": "Error", "error": str(e)}
+            reference_bytes = base64.b64decode(reference_image.split(',')[1]) #Same as above removes the first part
+
+            reference_np = face_recognition.api.load_image_file(io.BytesIO(reference_bytes))
+
+            reference_encodings = face_recognition.face_encodings(reference_np)
+            print(face_encodings)
+
+            results = face_recognition.compare_faces(reference_encodings, face_encodings[0]) # Compares the two faces, not sure why one is [0] but i trialed and errored and this is what worked
+        
+            if results[0]: # Should only be here if match
+                name = document["name"]
+                return "Face Recognized! Hello " + name # Returns name from mongo
+        return "Face Not Recognized" # Not recognized
+
