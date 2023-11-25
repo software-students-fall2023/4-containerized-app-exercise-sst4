7aac96cd8b37ba35f1acf3f83305b75980ddc446
tinybitofheaven <abc9295@nyu.edu>
2023-11-25 01:44:36 -0500

    initial dockerfiles + adjustments for web-app docker setup

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,10 @@
 """
 This is the app.py boilerplate
 """
+import os
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
@@ -15,4 +15,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
