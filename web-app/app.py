a7e76fc807c4003ae04d095f9a823cb65f0cd409
BradFeng02 <bradfeng02@gmail.com>
2023-11-25 12:22:52 -0500

    black and lint

--- web-app/app.py
+++ web-app/app.py
@@ -4,6 +4,7 @@ This is the app.py boilerplate
 from time import sleep
 from base64 import b64decode
 from io import BytesIO
+from PIL import Image
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -14,30 +15,33 @@ def index():
     """main page (only page)"""
     return render_template("index.html")
 
-@app.route("/login", methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
     """login. POST with photo in body."""
     # take image from body
+
     bodystr = request.get_data(cache=False, as_text=True, parse_form_data=False)
-    imgdata = bodystr.split(';')[1].split(',')[1]
+    imgdata = bodystr.split(";")[1].split(",")[1]
     # use with PIL:
-    # with Image.open(BytesIO(b64decode(imgdata))) as im:
-    #     im.show()
+    with Image.open(BytesIO(b64decode(imgdata))) as im:
+        print(im.size)
+        # im.show()
 
     # @TODO send image to ML client
-    sleep(5)
+    sleep(2)
 
     # dummy return @TODO
-    status = 'ok'
+    status = "ok"
     username = "TestUser123"
     sid = "132uygsfd76f12"
     content = "this is my private diary!!!\n~~~~\nI put all \
         my secrets here and they will be locked with my face."
     return {
-        'status': status,
-        'username': username,
-        'sid': sid,
-        'content': content,
+        "status": status,
+        "username": username,
+        "sid": sid,
+        "content": content,
     }
 
 
